c2e0d4276d787ceecc79da41f4d30c48bc1de12b
MrScautHD <MrScautHD5@gmail.com>
2022-05-28 14:24:19 +0200

    Added Paintings

--- src/main/java/net/mrscauthd/beyond_earth/registries/PaintingsRegistry.java
+++ src/main/java/net/mrscauthd/beyond_earth/registries/PaintingsRegistry.java
@@ -12,9 +12,18 @@ public class PaintingsRegistry {
 	public static final DeferredRegister<Motive> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, BeyondEarthMod.MODID);
 
 	/** PAINTINGS */
-	public static final RegistryObject<Motive> PAINTING_EARTH = PAINTINGS.register("painting_earth", () -> new Motive(32, 16));
-	public static final RegistryObject<Motive> PAINTING_GALAXY = PAINTINGS.register("painting_galaxy", () -> new Motive(32, 32));
-	public static final RegistryObject<Motive> PAINTING_MATH = PAINTINGS.register("painting_math", () -> new Motive(32, 16));
-	public static final RegistryObject<Motive> PAINTING_PLANET_MATH = PAINTINGS.register("painting_planet_math", () -> new Motive(64, 32));
-	public static final RegistryObject<Motive> PAINTING_VENUS = PAINTINGS.register("painting_venus", () -> new Motive(16, 16));
+	public static final RegistryObject<Motive> PAINTING_THE_MILKY_WAY = PAINTINGS.register("the_milky_way", () -> new Motive(64, 48));
+	public static final RegistryObject<Motive> PAINTING_SUN = PAINTINGS.register("sun", () -> new Motive(80, 80));
+	public static final RegistryObject<Motive> PAINTING_EARTH = PAINTINGS.register("earth", () -> new Motive(32, 32));
+	public static final RegistryObject<Motive> PAINTING_MOON = PAINTINGS.register("moon", () -> new Motive(16, 16));
+	public static final RegistryObject<Motive> PAINTING_MARS = PAINTINGS.register("mars", () -> new Motive(32, 32));
+	public static final RegistryObject<Motive> PAINTING_MERCURY = PAINTINGS.register("mercury", () -> new Motive(16, 16));
+	public static final RegistryObject<Motive> PAINTING_VENUS = PAINTINGS.register("venus", () -> new Motive(32, 32));
+	public static final RegistryObject<Motive> PAINTING_NEPTUNE = PAINTINGS.register("neptune", () -> new Motive(48, 48));
+	public static final RegistryObject<Motive> PAINTING_SATURN = PAINTINGS.register("saturn", () -> new Motive(64, 48));
+	public static final RegistryObject<Motive> PAINTING_JUPITER = PAINTINGS.register("jupiter", () -> new Motive(48, 48));
+	public static final RegistryObject<Motive> PAINTING_PLUTO = PAINTINGS.register("pluto", () -> new Motive(16, 16));
+	public static final RegistryObject<Motive> PAINTING_URANUS = PAINTINGS.register("uranus", () -> new Motive(48, 48));
+	public static final RegistryObject<Motive> PAINTING_PROXIMA_CENTAURY = PAINTINGS.register("proxima_centaury", () -> new Motive(64, 64));
+	public static final RegistryObject<Motive> PAINTING_GLACIO = PAINTINGS.register("glacio", () -> new Motive(32, 32));
 }
