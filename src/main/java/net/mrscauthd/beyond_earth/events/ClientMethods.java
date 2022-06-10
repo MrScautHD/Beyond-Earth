530c90631bb374e3b42263a9b4d1714ac4a48f48
MrScautHD <MrScautHD5@gmail.com>
2022-06-10 16:25:21 +0200

    r

--- src/main/java/net/mrscauthd/beyond_earth/events/ClientMethods.java
+++ src/main/java/net/mrscauthd/beyond_earth/events/ClientMethods.java
@@ -22,8 +22,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.mrscauthd.beyond_earth.BeyondEarth;
-import net.mrscauthd.beyond_earth.renderers.armors.JetSuitModel;
-import net.mrscauthd.beyond_earth.renderers.armors.SpaceSuitModel;
+import net.mrscauthd.beyond_earth.entities.renderers.armors.JetSuitModel;
+import net.mrscauthd.beyond_earth.entities.renderers.armors.SpaceSuitModel;
 import net.mrscauthd.beyond_earth.registries.ItemsRegistry;
 
 @OnlyIn(Dist.CLIENT)
