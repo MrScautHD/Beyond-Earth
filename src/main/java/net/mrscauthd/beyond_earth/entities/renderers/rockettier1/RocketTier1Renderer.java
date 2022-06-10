7446d14e62c1870d369d67f60b10b5b170514ed1
MrScautHD <MrScautHD5@gmail.com>
2022-06-10 16:25:12 +0200

    changed renderers packet

---
+++ src/main/java/net/mrscauthd/beyond_earth/entities/renderers/rockettier1/RocketTier1Renderer.java
@@ -0,0 +1,24 @@
+package net.mrscauthd.beyond_earth.entities.renderers.rockettier1;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.mrscauthd.beyond_earth.BeyondEarth;
+import net.mrscauthd.beyond_earth.entities.RocketTier1Entity;
+import net.mrscauthd.beyond_earth.entities.renderers.VehicleRenderer;
+
+@OnlyIn(Dist.CLIENT)
+public class RocketTier1Renderer extends VehicleRenderer<RocketTier1Entity, RocketTier1Model<RocketTier1Entity>> {
+
+    public static final ResourceLocation TEXTURE = new ResourceLocation(BeyondEarth.MODID, "textures/vehicles/rocket_t1.png");
+
+    public RocketTier1Renderer(EntityRendererProvider.Context renderManagerIn) {
+        super(renderManagerIn, new RocketTier1Model<>(renderManagerIn.bakeLayer(RocketTier1Model.LAYER_LOCATION)), 0.5f);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(RocketTier1Entity p_114482_) {
+        return TEXTURE;
+    }
+}
