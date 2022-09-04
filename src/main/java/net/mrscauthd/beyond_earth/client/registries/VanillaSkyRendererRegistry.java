600063a693b6ab00aa0a98592257500996713773
MrScautHD <MrScautHD5@gmail.com>
2022-09-04 23:06:12 +0200

    Update VanillaSkyRendererRegistry.java

--- src/main/java/net/mrscauthd/beyond_earth/client/registries/VanillaSkyRendererRegistry.java
+++ src/main/java/net/mrscauthd/beyond_earth/client/registries/VanillaSkyRendererRegistry.java
@@ -13,11 +13,13 @@ import net.mrscauthd.beyond_earth.client.renderers.sky.EarthSky;
 @Mod.EventBusSubscriber(modid = BeyondEarth.MODID, value = Dist.CLIENT)
 public class VanillaSkyRendererRegistry {
 
+    public static final DimensionSpecialEffects EARTH_EFFECT = new EarthSky(192.0F, true, DimensionSpecialEffects.SkyType.NORMAL, false, false);
+
     @SubscribeEvent
     public static void overrideVanillaEffects(LevelEvent.Load event) {
         if (event.getLevel() instanceof ClientLevel clientLevel) {
             if (clientLevel.dimension() == Level.OVERWORLD) {
-                clientLevel.effects = new EarthSky(192.0F, true, DimensionSpecialEffects.SkyType.NORMAL, false, false);
+                clientLevel.effects = EARTH_EFFECT;
             }
         }
     }
