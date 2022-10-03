e4bab19eb6ec3ec99a3af5829f1488d5a66f9e33
Thutmose <Thutmose@users.noreply.github.com>
2022-10-03 19:05:05 -0400

    Update Jei.java
    template for JEI module once recipes are in

--- src/main/java/net/mrscauthd/beyond_earth/common/jei/Jei.java
+++ src/main/java/net/mrscauthd/beyond_earth/common/jei/Jei.java
@@ -1,6 +1,34 @@
 package net.mrscauthd.beyond_earth.common.jei;
 
-public class Jei {
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.ResourceLocation;
+import net.mrscauthd.beyond_earth.BeyondEarth;
 
-    //Happy birthday Fizz
+@JeiPlugin
+public class Jei implements IModPlugin {
+    
+    private static final ResourceLocation UID = new ResourceLocation(BeyondEarth.MODID, "jei");
+
+    @Override
+    public ResourceLocation getPluginUid()
+    {
+        return UID;
+    }
+    
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration)
+    {
+        // TODO Auto-generated method stub
+        IModPlugin.super.registerCategories(registration);
+    }
+    
+    @Override
+    public void registerRecipes(IRecipeRegistration registration)
+    {
+        // TODO Auto-generated method stub
+        IModPlugin.super.registerRecipes(registration);
+    }
 }
