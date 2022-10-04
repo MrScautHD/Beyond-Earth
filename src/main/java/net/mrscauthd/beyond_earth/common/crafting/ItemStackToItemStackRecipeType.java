2a30acc9a47c4d64444c59e660f167d2cd45a9b6
Thutmose <Thutmose@users.noreply.github.com>
2022-10-04 05:58:04 -0400

    moves the recipes inside common

---
+++ src/main/java/net/mrscauthd/beyond_earth/common/crafting/ItemStackToItemStackRecipeType.java
@@ -0,0 +1,9 @@
+package net.mrscauthd.beyond_earth.common.crafting;
+
+public class ItemStackToItemStackRecipeType<T extends ItemStackToItemStackRecipe> extends BeyondEarthRecipeType<T> {
+
+	public ItemStackToItemStackRecipeType(String name) {
+		super(name);
+	}
+
+}
