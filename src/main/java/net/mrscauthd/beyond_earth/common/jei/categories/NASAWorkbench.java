c44946f458c35e5cbc3cec5b5ed37a3bcfe0662f
Thutmose <Thutmose@users.noreply.github.com>
2022-10-04 22:25:30 -0400

    Update NASAWorkbench.java
    JEI layout for this, note that the tags are not set right, so these recipes show barrier blocks as invalid for now

--- src/main/java/net/mrscauthd/beyond_earth/common/jei/categories/NASAWorkbench.java
+++ src/main/java/net/mrscauthd/beyond_earth/common/jei/categories/NASAWorkbench.java
@@ -1,15 +1,20 @@
 package net.mrscauthd.beyond_earth.common.jei.categories;
 
+import java.util.Collections;
 import java.util.List;
+
+import org.apache.commons.compress.utils.Lists;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.resources.language.I18n;
@@ -17,9 +22,11 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.mrscauthd.beyond_earth.BeyondEarth;
+import net.mrscauthd.beyond_earth.common.crafting.RocketPart;
 import net.mrscauthd.beyond_earth.common.crafting.WorkbenchingRecipe;
 import net.mrscauthd.beyond_earth.common.jei.Jei;
 import net.mrscauthd.beyond_earth.common.registries.ItemsRegistry;
+import net.mrscauthd.beyond_earth.common.registries.RocketPartsRegistry;
 
 public class NASAWorkbench implements IRecipeCategory<WorkbenchingRecipe> {
     public static final ResourceLocation GUI = new ResourceLocation(BeyondEarth.MODID, "textures/jei/jei_gui_1.png");
@@ -74,10 +81,84 @@ public class NASAWorkbench implements IRecipeCategory<WorkbenchingRecipe> {
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, WorkbenchingRecipe recipe, IFocusGroup focuses) {
-//        IRecipeSlotBuilder inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 30, 19);
-//        inputStack.addIngredientsUnsafe(recipe.getIngredients());
-//        
-        
+
+        RocketPart nose = RocketPartsRegistry.ROCKET_PART_NOSE.get();
+        RocketPart body = RocketPartsRegistry.ROCKET_PART_BODY.get();
+        RocketPart tank = RocketPartsRegistry.ROCKET_PART_TANK.get();
+        RocketPart left = RocketPartsRegistry.ROCKET_PART_FIN_LEFT.get();
+        RocketPart right = RocketPartsRegistry.ROCKET_PART_FIN_RIGHT.get();
+        RocketPart engine = RocketPartsRegistry.ROCKET_PART_ENGINE.get();
+
+        int dx = -7;
+        int dy = -9;
+
+        List<ItemStack> stacks = Lists.newArrayList();
+
+        IRecipeSlotBuilder inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 40 + dx, 18 + dy);
+        var list = recipe.getParts().getOrDefault(nose, Collections.emptyList());
+        stacks.clear();
+        list.forEach(e -> {
+            for (var i : e.getItems())
+                stacks.add(i);
+        });
+        inputStack.addItemStacks(stacks);
+
+        list = recipe.getParts().getOrDefault(body, Collections.emptyList());
+        stacks.clear();
+        list.forEach(e -> {
+            for (var i : e.getItems())
+                stacks.add(i);
+        });
+        for (int x = 0; x < 2; x++)
+            for (int y = 0; y < 3; y++) {
+                inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 31 + dx + x * 18, 36 + dy + y * 18);
+                inputStack.addItemStacks(stacks);
+            }
+
+        list = recipe.getParts().getOrDefault(tank, Collections.emptyList());
+        stacks.clear();
+        list.forEach(e -> {
+            for (var i : e.getItems())
+                stacks.add(i);
+        });
+        for (int x = 0; x < 2; x++) {
+            inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 31 + dx + x * 18, 90 + dy);
+            inputStack.addItemStacks(stacks);
+        }
+
+        list = recipe.getParts().getOrDefault(left, Collections.emptyList());
+        stacks.clear();
+        list.forEach(e -> {
+            for (var i : e.getItems())
+                stacks.add(i);
+        });
+        for (int y = 0; y < 2; y++) {
+            inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 13 + dx, 90 + dy + y * 18);
+            inputStack.addItemStacks(stacks);
+        }
+
+        list = recipe.getParts().getOrDefault(right, Collections.emptyList());
+        stacks.clear();
+        list.forEach(e -> {
+            for (var i : e.getItems())
+                stacks.add(i);
+        });
+        for (int y = 0; y < 2; y++) {
+            inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 67 + dx, 90 + dy + y * 18);
+            inputStack.addItemStacks(stacks);
+        }
+
+        inputStack = builder.addSlot(RecipeIngredientRole.INPUT, 40 + dx, 108 + dy);
+        list = recipe.getParts().getOrDefault(engine, Collections.emptyList());
+        stacks.clear();
+        list.forEach(e -> {
+            for (var i : e.getItems())
+                stacks.add(i);
+        });
+        inputStack.addItemStacks(stacks);
+
+        IRecipeSlotBuilder outputStack = builder.addSlot(RecipeIngredientRole.OUTPUT, 86, 28);
+        outputStack.addItemStack(recipe.getOutput());
     }
 
 }
