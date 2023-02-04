93ebec8977d95310a143e602839f47adc18a4e15
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2023-02-04 09:33:18 -0500

    - fix soul soil configured feature on moon - fix structure generation - fix iron rod tag in translation - fix oxygen container

--- src/main/java/net/mrscauthd/beyond_earth/common/world/ModConfiguredFeature.java
+++ src/main/java/net/mrscauthd/beyond_earth/common/world/ModConfiguredFeature.java
@@ -78,7 +78,6 @@ public class ModConfiguredFeature {
             OreConfiguration.target(new BlockMatchTest(BlockRegistry.VENUS_STONE.get()), BlockRegistry.VENUS_GOLD_ORE.get().defaultBlockState())));
 
 
-
     // GLACIO
     public static final RegistryObject<ConfiguredFeature<?, ?>> GLACIO_COAL_ORE = CONFIGURED_FEATURES.register("glacio_coal_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(GLACIO_COAL_ORE_REPLACEABLES.get(), 17)));
     public static final RegistryObject<ConfiguredFeature<?, ?>> GLACIO_COPPER_ORE = CONFIGURED_FEATURES.register("glacio_copper_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(GLACIO_COPPER_ORE_REPLACEABLES.get(), 17)));
@@ -100,7 +99,7 @@ public class ModConfiguredFeature {
     public static final RegistryObject<ConfiguredFeature<?, ?>> MOON_DESH_ORE = CONFIGURED_FEATURES.register("moon_desh_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(MOON_DESH_ORE_REPLACEABLES.get(), 9)));
     public static final RegistryObject<ConfiguredFeature<?, ?>> MOON_ICE_SHARD_ORE = CONFIGURED_FEATURES.register("moon_ice_shard_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(MOON_ICE_SHARD_ORE_REPLACEABLES.get(), 10)));
     public static final RegistryObject<ConfiguredFeature<?, ?>> MOON_IRON_ORE = CONFIGURED_FEATURES.register("moon_iron_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(MOON_IRON_ORE_REPLACEABLES.get(), 11)));
-    public static final RegistryObject<ConfiguredFeature<?,?>> MOON_SOUL_SOIL_CONFIGURED = CONFIGURED_FEATURES.register("moon_soul_soil", () -> new ConfiguredFeature(Feature.ORE, new OreConfiguration(MOON_SOUL_SOIL_REPLACEABLES.get(), 60)));
+    public static final RegistryObject<ConfiguredFeature<?,?>> MOON_SOUL_SOIL_CONFIGURED = CONFIGURED_FEATURES.register("moon_soul_soil", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(MOON_SOUL_SOIL_REPLACEABLES.get(), 60)));
 
     // VENUS
     public static final RegistryObject<ConfiguredFeature<?, ?>> VENUS_CALORITE_ORE = CONFIGURED_FEATURES.register("venus_calorite_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(VENUS_CALORITE_ORE_REPLACEABLES.get(), 8)));
