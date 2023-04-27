544533f3de8c96f252ea1e5c01d82c796a882d45
TATHAN <costa.tathan@gmail.com>
2023-04-27 22:23:21 +0200

    Fix Glacio Tree Sapling

--- src/main/java/net/mrscauthd/beyond_earth/common/registries/ItemsRegistry.java
+++ src/main/java/net/mrscauthd/beyond_earth/common/registries/ItemsRegistry.java
@@ -215,7 +215,7 @@ public class ItemsRegistry {
     public static final RegistryObject<BlockItem> GLACIO_WOOD_ITEM = ITEMS.register("glacio_wood", () -> new BlockItem(BlockRegistry.GLACIO_WOOD.get(), new Item.Properties()));
     public static final RegistryObject<BlockItem> GLACIO_WOOD_LEAVES_ITEM = ITEMS.register("glacio_tree_leaves", () -> new BlockItem(BlockRegistry.GLACIO_WOOD_LEAVES.get(), new Item.Properties()));
     public static final RegistryObject<BlockItem> GLACIO_WOOD_PLANKS_ITEM = ITEMS.register("glacio_wood_planks", () -> new BlockItem(BlockRegistry.GLACIO_WOOD_PLANKS.get(), new Item.Properties()));
-    public static final RegistryObject<BlockItem> GLACIO_TREE_SAPLING_ITEM = ITEMS.register("glacio_tree_spaling", () -> new BlockItem(BlockRegistry.GLACIO_TREE_SAPLING.get(), new Item.Properties()));
+    public static final RegistryObject<BlockItem> GLACIO_TREE_SAPLING_ITEM = ITEMS.register("glacio_tree_sapling", () -> new BlockItem(BlockRegistry.GLACIO_TREE_SAPLING.get(), new Item.Properties()));
 
     // SAND BLOCK ITEMS
     public static final RegistryObject<BlockItem> MOON_SAND_ITEM = ITEMS.register("moon_sand", () -> new BlockItem(BlockRegistry.MOON_SAND.get(), new Item.Properties()));
